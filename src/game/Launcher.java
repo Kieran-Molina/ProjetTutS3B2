7a99fa7f7306cb08a0c2dfca03c669bec14bb54b
Kieran-Molina <kierandu70@hotmail.fr>
2015-10-01 23:02:13 +0200

    1er build (un peu vide, mais bon... faut bien commencer par quelque chose) de notre projet tut.

--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -1,10 +1,8 @@
 package game;
 
-/**
- * Created by Kiéran on 01/10/2015.
- */
+
 public class Launcher {
     public static void main(String args[]){
-        System.out.println("Un bon debut ! ");
+        System.out.println("Un bon debut !");
     }
 }
