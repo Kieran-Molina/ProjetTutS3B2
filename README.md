81df86d4e8ac279a023b37f7cd49ca44170f4cec
nico <horrorhotline@hotmail.com>
2015-10-02 14:07:04 +0200

    rien juste un test

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ IUT Belfort, 2015-2016
 Projet tuteuré du jeu de stratégie.
 work in progress...
 Nicolas, ok, accepté fichier édité.
+test plug in
+This is not greenwich village
+
--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -4,5 +4,8 @@ package game;
 public class Launcher {
     public static void main(String args[]){
         System.out.println("Un bon debut !");
-    }
+
+
+        System.out.println("test check !");
+
 }
