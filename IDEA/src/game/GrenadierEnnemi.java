4cdd5049cd102297f1ede8f3d3ddb57212981396
Darkbattor <val.masoni@laposte.net>
2016-01-07 19:40:31 +0100

    Ajout infanterie/grenadier ennemi + les classes associées

---
+++ IDEA/src/game/GrenadierEnnemi.java
@@ -0,0 +1,31 @@
+package game;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by nico on 06/01/16.
+ */
+public class GrenadierEnnemi extends Unite {
+
+    private static final ImageIcon img_UP = new ImageIcon("IDEA/Images/Tactical/Unite/GrenadierEnnemi/Normal/Déplacement_UP/GrenadierEnnemi_Deplacement_1.png");
+    private static final ImageIcon img_RIGHT = new ImageIcon("IDEA/Images/Tactical/Unite/GrenadierEnnemi/Normal/Déplacement_RIGHT/GrenadierEnnemi_Deplacement_1.png");
+    private static final ImageIcon img_DOWN = new ImageIcon("IDEA/Images/Tactical/Unite/GrenadierEnnemi/Normal/Déplacement_DOWN/GrenadierEnnemi_Deplacement_1.png");
+    private static final ImageIcon img_LEFT = new ImageIcon("IDEA/Images/Tactical/Unite/GrenadierEnnemi/Normal/Déplacement_LEFT/GrenadierEnnemi_Deplacement_1.png");
+    public static final int UP = 1, RIGHT = 2, DOWN = 3, LEFT = 4;
+
+
+
+
+    public GrenadierEnnemi(int posX, int posY, int dep, boolean ami) {
+
+        super(posX, posY, dep, ami);
+        loadImage("IDEA/Images/Tactical/Unite/GrenadierEnnemi/Normal/Déplacement_LEFT/GrenadierEnnemi_Deplacement_1.png");
+        getImageDimensions();
+    }
+
+
+
+
+
+}
