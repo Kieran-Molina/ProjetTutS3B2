fc808e5168344c353c79febdaed65a03ec18370b
nekosan99 <horrorhotline@hotmail.com>
2016-01-08 21:36:29 +0100

    modif Bformation

--- IDEA/src/game/BFormation.java
+++ IDEA/src/game/BFormation.java
@@ -6,13 +6,23 @@ import javax.swing.*;
  * Created by nico on 04/01/16.
  */
 public class BFormation extends Batiment {
+    protected boolean isSelect;
+
 
     public BFormation(int lx,int ly) {
         super(lx,ly);
-
+        isSelect=false;
         loadImage("IDEA/Images/Tactical/Batiment/BatimentFormation.png");
         getImageDimensions();
 
+    }
+
+    public boolean getSelect(){return isSelect;}
+
+    public void setSelect(){
+        if(isSelect==true){
+            isSelect=false;
+        }else{isSelect=true;}
     }
 }
 
