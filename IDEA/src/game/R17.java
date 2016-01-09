9b66b9c46634d48fba3c745168c880f9d94913ea
Maxime Vavasseur <Maxime Vavasseur>
2016-01-09 21:59:34 +0100

    modification force autre unite

--- IDEA/src/game/R17.java
+++ IDEA/src/game/R17.java
@@ -3,6 +3,8 @@ package game;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 
+import static java.lang.Math.random;
+
 /**
  * Created by nico on 03/01/16.
  */
@@ -15,7 +17,7 @@ public class R17 extends Unite {
             loadImage("IDEA/Images/Tactical/Unite/Renault_FT17/Normal/Déplacement_LEFT/Renault_FT_17_Deplacement_1.png");}//if pour savoir que sprite selon l'armée
         else{loadImage("IDEA/Images/Tactical/Unite/Renault_FT17/Normal/Déplacement_RIGHT/Renault_FT_17_Deplacement_1.png");}
         getImageDimensions();
-        force=8;
+        force=(int)(125+random()*(125-80));
         prix=500;
     }
 
