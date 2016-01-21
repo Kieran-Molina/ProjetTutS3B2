9cb37c8b637f7169951ba060aa15135eb82b4133
nekosan99 <horrorhotline@hotmail.com>
2016-01-21 21:26:40 +0100

    help

--- IDEA/src/game/Joueur.java
+++ IDEA/src/game/Joueur.java
@@ -15,8 +15,41 @@ public class Joueur {
         armee = new ArrayList<Unite>();
         bat = new ArrayList<Batiment>();
         Argent = Argent+x;
+    }
+
+    public void videArme(){ armee.clear();}//vidage de la liste
+
+    public void addUnite(Unite u){armee.add(u);}
+
+    public Unite getUnite(int index){
+        return armee.get(index);
 
     }
+
+    public void removeUnite(int index){
+        armee.remove(index);
+    }
+
+    public void videbat(){ bat.clear();}//vidage de la liste
+
+    public void addBatiment(Batiment b){bat.add(b);}
+
+    public Batiment getBatment(int index){
+        return bat.get(index);
+    }
+
+    public void removeBatiment(int index){
+        bat.remove(index);
+    }
+
+    public int getArgent(){
+        return Argent;
+    }
+    public void setARgent(int somme){
+        Argent=Argent+somme;
+    }
+
+
 
 
 
